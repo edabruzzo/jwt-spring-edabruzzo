0deff4ffa815af8578ab6f8086c1abd1d2cc5aae
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-29 18:01:43 -0300

    Cria serviço que implementa userdetails

---
+++ src/main/java/br/com/abruzzo/jwt_spring/service/UserDetailServiceImplementation.java
@@ -0,0 +1,48 @@
+package br.com.abruzzo.jwt_spring.service;
+
+import br.com.abruzzo.jwt_spring.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserDetailServiceImplementation implements UserDetailsService {
+
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserDTO userDTO = findUserByUsername(username);
+        if(userDTO == null)
+            throw new UsernameNotFoundException(username);
+        return new User(userDTO.getUsername(),userDTO.getPassword(), Collections.emptyList());
+    }
+
+
+    private UserDTO findUserByUsername(String username) {
+        UserDTO user = new UserDTO();
+        user.setUsername("admin");
+        user.setPassword(this.bCryptPasswordEncoder.encode("admin"));
+        return user;
+    }
+
+
+    public List<UserDTO> listUsers(){
+        ArrayList<UserDTO> usersList= new ArrayList<>();
+        usersList.add(findUserByUsername("admin"));
+        return usersList;
+    }
+
+
+}
