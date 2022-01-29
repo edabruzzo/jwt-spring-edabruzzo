06242e84adee773f27cb7f1ba44ee25889def06f
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-29 18:01:01 -0300

    Cria data transfer object para User

---
+++ src/main/java/br/com/abruzzo/jwt_spring/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package br.com.abruzzo.jwt_spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String username;
+    private String password;
+
+}
