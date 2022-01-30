122b4d765c09b7968b86b70f91d30fa847a7fe45
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-29 21:42:34 -0300

    Adiciona construtor privado na classe de utilidade SecurityConstraints para impedir que seja instanciada.

--- src/main/java/br/com/abruzzo/jwt_spring/controller/SecurityConstraints.java
+++ src/main/java/br/com/abruzzo/jwt_spring/controller/SecurityConstraints.java
@@ -5,6 +5,11 @@ import java.util.concurrent.TimeUnit;
 
 public class SecurityConstraints {
 
+
+    private SecurityConstraints() {
+        throw new IllegalStateException("Utility class should not be instantiated");
+    }
+
     public static final String SECRET = UUID.randomUUID().toString();
     public static final long EXPIRATION_TIME = TimeUnit.DAYS.toMillis(10);
     public static final String TOKEN_PREFIX = "Bearer ";
