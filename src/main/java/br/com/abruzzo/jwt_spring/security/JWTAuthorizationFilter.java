99659578d9b9defb25c2b3b792541d5d33c487dd
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-31 16:35:14 -0300

    Adiciona filtro de validação de usuário

--- src/main/java/br/com/abruzzo/jwt_spring/security/JWTAuthorizationFilter.java
+++ src/main/java/br/com/abruzzo/jwt_spring/security/JWTAuthorizationFilter.java
@@ -1,11 +1,9 @@
 package br.com.abruzzo.jwt_spring.security;
 
-import org.springframework.core.log.LogMessage;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -14,6 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
@@ -30,15 +29,41 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
-                                    FilterChain chain) throws IOException, ServletException,AusenciaTokenAutenticacao {
+                                    FilterChain chain) throws ServletException, IOException {
 
         String header = request.getHeader(SecurityConstraints.HEADER_TOKEN);
 
-        if(header == null || header.startsWith(SecurityConstraints.TOKEN_PREFIX))
-            throw new AusenciaTokenAutenticacao("Não foi fornecido token autenticação no request", this.logger);
+        if(header == null || header.startsWith(SecurityConstraints.TOKEN_PREFIX)) {
+            try{
+                throw new AusenciaTokenAutenticacao("Não foi fornecido token autenticação no request", this.logger);
+            }catch(AusenciaTokenAutenticacao exception){
+                exception.printStackTrace();
+            }
+            chain.doFilter(request,response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+        chain.doFilter(request,response);
 
     }
 
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+
+        String token = request.getHeader(SecurityConstraints.HEADER_TOKEN);
+        if(token == null){
+            throw new AusenciaTokenAutenticacao("Token nulo");
+        }
+
+        String username = JWT.require(Algorithm.HMAC256(SecurityConstraints.SECRET.getBytes()))
+                .build()
+                .verify(token.replace(SecurityConstraints.TOKEN_PREFIX,""))
+                .getSubject();
+
+        return new UsernamePasswordAuthenticationToken(username,null,new ArrayList<>());
+
+    }
 
 
 }
