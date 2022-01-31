ddd9f680a9d23506ac53ce4dc84d58a499fd9a73
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-31 14:35:47 -0300

    Inclui anotação @Service no UserDetailServiceImplementation

--- src/main/java/br/com/abruzzo/jwt_spring/service/UserDetailServiceImplementation.java
+++ src/main/java/br/com/abruzzo/jwt_spring/service/UserDetailServiceImplementation.java
@@ -7,11 +7,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+@Service
 public class UserDetailServiceImplementation implements UserDetailsService {
 
 
