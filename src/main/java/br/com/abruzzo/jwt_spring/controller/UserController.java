8e8c3e7983945b9f18b87346690a014b388b0b0d
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-31 14:35:03 -0300

    Adiciona endpoints no RestController de usuários

--- src/main/java/br/com/abruzzo/jwt_spring/controller/UserController.java
+++ src/main/java/br/com/abruzzo/jwt_spring/controller/UserController.java
@@ -5,18 +5,21 @@ import br.com.abruzzo.jwt_spring.dto.UserDTO;
 import br.com.abruzzo.jwt_spring.service.UserDetailServiceImplementation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RequestMapping("/users")
 public class UserController {
 
 
     private final UserDetailServiceImplementation userDetailsService;
 
-
+    @GetMapping
     public List<UserDTO> listAllUsers(){
         return this.userDetailsService.listUsers();
     }
