3a9abab531129c54e322c5a0d75e59d5f8bed3c8
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2022-01-31 16:46:07 -0300

    Corrige nome do parâmetro Authorization enviado no header dos requests.

--- src/main/java/br/com/abruzzo/jwt_spring/security/SecurityConstraints.java
+++ src/main/java/br/com/abruzzo/jwt_spring/security/SecurityConstraints.java
@@ -13,7 +13,7 @@ public class SecurityConstraints {
     public static final String SECRET = UUID.randomUUID().toString();
     public static final long EXPIRATION_TIME = TimeUnit.DAYS.toMillis(10);
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String HEADER_TOKEN = "Authorizatin";
+    public static final String HEADER_TOKEN = "Authorization";
     public static final String LOGIN_URL = "/login";
     public static final String STATUS_URL = "/status";
 
